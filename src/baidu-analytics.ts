90a7a60749a4b47d896a63a8dd43e7ab85a4eee2
304303454 <112739973+304303454@users.noreply.github.com>
2023-09-01 17:35:07 +0800

    Create baidu-analytics.ts

---
+++ src/baidu-analytics.ts
@@ -0,0 +1,18 @@
+// src/baidu-analytics.ts
+declare let _hmt: any[];
+
+interface BaiduAnalytics {
+  push: (...args: any[]) => void;
+}
+
+export function useBaiduAnalytics(): BaiduAnalytics {
+  const push = (...args: any[]) => {
+    if (typeof _hmt !== 'undefined') {
+      _hmt.push(args);
+    }
+  };
+
+  return {
+    push,
+  };
+}
